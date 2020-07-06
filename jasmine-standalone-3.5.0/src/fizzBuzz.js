bb3fe11548cbae3be957abb8a749ed7faaacdae9
Matthew Lock <matthew_lock20@hotmail.com>
2020-07-06 20:04:14 +0100

    Javabuzz Javscript Introduction

---
+++ jasmine-standalone-3.5.0/src/fizzBuzz.js
@@ -0,0 +1,27 @@
+class Javabuzz {
+
+  isDivisibleByFifteen(number) {
+    return this._isDivisibleBy(number, 15);
+  }
+  isDivisibleByThree(number) {
+    return this._isDivisibleBy(number, 3);
+  }
+  isDivisibleByFive(number) {
+    return this._isDivisibleBy(number, 5 );
+  }
+  _isDivisibleBy(number, divisor) {
+    return (number % divisor === 0);
+  }
+  says(number) {
+    if (this.isDivisibleByFifteen(number)) {
+      return "Javabuzz";
+    }
+    if (this.isDivisibleByThree(number)) {
+      return "Java";
+    }
+    if (this.isDivisibleByFive(number)) {
+      return "Buzz";
+    }
+    return number;
+  }
+}
